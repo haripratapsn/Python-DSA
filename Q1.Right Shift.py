ffcb873c308c4b0e779402ede473235a43998bcb
Hari Pratap SN <haripratapsn2003@gmail.com>
2025-09-09 04:05:43 +0530

    Code Added

---
+++ Q1.Right Shift.py
@@ -0,0 +1,12 @@
+# Solid Square Pattern
+
+
+sides=int(input("Enter the length of sides: "))
+
+for i in range(sides):
+    for j in range(sides):
+        print("*",end="")
+    print()
+
+# for i in range(sides):
+#     print("*"*sides)
